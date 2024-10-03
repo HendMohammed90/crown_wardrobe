ef47ad7e375b3090b8f19d6172507958775cc8f2
HendMohammed90 <hend.odesk.tasks@gmail.com>
2024-10-03 09:29:00 +0300

    add logo as svg and styling for navigation

--- src/App.tsx
+++ src/App.tsx
@@ -11,5 +11,5 @@ function App() {
     </Routes>
   );
 }
-
+ 
 export default App
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,11 +1,23 @@
-import { Outlet } from "react-router-dom"
+import { Link, Outlet } from "react-router-dom"
 import "./Navigation.scss"
+import { Fragment } from "react"
+// import CrownLogo from "../../assets/crown.svg"
+import CrownLogo from "../../assets/crown.svg?react"; 
 const Navigation = () => {
   return (
-    <>
-    <div>Navigation Bar</div>
+    <Fragment>
+      <div className="navigation">
+        <Link to={"/"} className="logo-container">
+          <CrownLogo />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to={"/home"}>
+            shop
+          </Link>
+        </div>
+      </div>
     <Outlet />
-    </>
+    </Fragment>
     )
 }
 
