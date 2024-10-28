3aa6726a908d3c9e5a3b64cfc2ff9386f095fdd3
HendMohammed90 <hend.odesk.tasks@gmail.com>
2024-10-28 21:04:11 +0300

    feat:add firebase and initialize it

---
+++ src/utils/firebase/firebase.utils.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA0AU4gAZ0xKEh7tDGmPM4cl4npleF4taQ",
+  authDomain: "crown-wardrobe-db.firebaseapp.com",
+  projectId: "crown-wardrobe-db",
+  storageBucket: "crown-wardrobe-db.appspot.com",
+  messagingSenderId: "942804660202",
+  appId: "1:942804660202:web:8291cedfcd27070dbbcc93"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
