5ae9d680df03b52d965c4aa0f66e0f111fca75c8
HendMohammed90 <hend.odesk.tasks@gmail.com>
2024-10-28 21:39:16 +0300

    feat:add signInWithGooglePopup methode to signIn page

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,13 +1,18 @@
-
+import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
 
 // type SignInProps = {
   // props go here
 // };
 
 export function SignIn() {
+  const logInWithGoogle = async () => {
+    const response = await signInWithGooglePopup();
+    console.log(response);
+  }
   return (
     <>
       <h1>SignIn</h1>
+      <button onClick={logInWithGoogle}> signIn with google</button>
     </>
   );
 }
--- src/utils/firebase/firebase.utils.ts
+++ src/utils/firebase/firebase.utils.ts
@@ -1,8 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
+import {getAuth , signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyA0AU4gAZ0xKEh7tDGmPM4cl4npleF4taQ",
@@ -14,4 +12,16 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const firebaseApp = initializeApp(firebaseConfig);
+
+// Initialize Authorization
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt : 'select_account'
+});
+
+export const auth = getAuth();
+
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider) ;
