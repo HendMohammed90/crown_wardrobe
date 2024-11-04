b31a7174f8231dd25f7770eb35e38792d28a06b3
HendMohammed90 <hend.odesk.tasks@gmail.com>
2024-11-04 21:36:27 +0200

    feat:creat signIn & signUp pages using mongez/react-form

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,8 +1,9 @@
-import { signInWithGooglePopup, createUserDocFromAuth } from "../../utils/firebase/firebase.utils";
+import EmailInput from "../../shared/Components/EmailInput";
+import { signInWithGooglePopup, createUserDocFromAuth, createAuthUserWithEmailAndPassword } from "../../utils/firebase/firebase.utils";
+import PasswordInput from "../../shared/Components/PasswordInput";
+import { Form, FormSubmitOptions } from "@mongez/react-form";
+import { SignUp } from "../SignUp";
 
-// type SignInProps = {
-  // props go here
-// };
 
 export function SignIn() {
   const logInWithGoogle = async () => {
@@ -10,11 +11,52 @@ export function SignIn() {
     const user = await createUserDocFromAuth(response);
     console.log(`USER is ${JSON.stringify(user)}`)
     return user;
+
   }
+
+  const submitForm = async ({ values }: { values: { email: string; password: string; } }) => {
+    const user = {
+      email: values.email,
+      password: values.password,
+    };
+
+    console.log(user);
+    const result = await createAuthUserWithEmailAndPassword(user.email, user.password);
+    console.log(`result is ${JSON.stringify(result)}`);
+
+  };
+
+
   return (
     <>
-      <h1>SignIn</h1>
-      <button onClick={logInWithGoogle}> signIn with google</button>
+      <h1>Sign In Page</h1>
+      <Form
+        onSubmit={(options: FormSubmitOptions) => {
+          const values = options.values as { email: string; password: string; };
+          submitForm({ values: values });
+        }}
+      >
+        <EmailInput
+          name="email"
+          type="email"
+          required
+          labelText="Email"
+          className="group"
+          inputClassName="form-input"
+        />
+        <PasswordInput
+          type="text"
+          name="password"
+          minLength={8}
+          required
+          labelText="Password"
+          className="group"
+          inputClassName="form-input"
+        />
+        <button onClick={logInWithGoogle}> signIn with google</button>
+        <button type="submit"> signIn with email & password</button>
+      </Form>
+      <SignUp />
     </>
   );
 }
---
+++ src/pages/SignUp/SignUp.tsx
@@ -0,0 +1,82 @@
+import TextInput from "../../shared/Components/TextInput";
+import EmailInput from "../../shared/Components/EmailInput";
+import PasswordInput from "../../shared/Components/PasswordInput";
+import { Form, FormSubmitOptions } from "@mongez/react-form";
+import { createAuthUserWithEmailAndPassword, createUserDocFromAuth } from "../../utils/firebase/firebase.utils";
+import "./signUp.scss"
+import Button from "../../shared/Components/Button";
+
+export function SignUp() {
+  const submitForm = async ({ values }: { values: { displayName: string ; email: string; password: string; confirmPassword: string } }) => {
+    const user = {
+      displayName: values.displayName,
+      email: values.email,
+      password: values.password,
+      confirmPassword: values.confirmPassword
+    };
+    if(user.password !== user.confirmPassword){
+      alert('Password don\'t match');
+      return;
+    }
+    try {
+      const result = await createAuthUserWithEmailAndPassword(user.email, user.password);
+      if (!result) {
+        console.error('Failed to create user');
+        return;
+      }
+      console.log(`result after sign up ${JSON.stringify(result)}`);
+      await createUserDocFromAuth(result , user);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  
+  return (
+    <div className="sign-up-container">
+      <h2>Don't have an account</h2>
+      <span>Sign Up with your email and password </span>
+      <Form onSubmit={(options: FormSubmitOptions) => {
+        const values = options.values as { displayName: string; email: string; password: string; confirmPassword: string; };
+        submitForm({ values: values });
+        options.form.reset()
+      }}>
+        <TextInput         
+        name="displayName"
+        type="text"
+        required 
+        labelText="Display Name"
+        className="group"
+        inputClassName="form-input"/>
+      <EmailInput
+        name="email"
+        type="email"
+        required
+        labelText="Email"
+        className="group"
+        inputClassName="form-input"
+      />
+      <PasswordInput
+        type="text"
+        name="password"
+        minLength={8}
+        required
+        labelText="Password"
+        className="group"
+        inputClassName="form-input"
+      />
+      <PasswordInput
+        type="text"
+        name="confirmPassword"
+        match="password"
+        minLength={8}
+        required
+        labelText="confirm Password"
+        className="group"
+        inputClassName="form-input"
+      />  
+      <Button type="submit" buttonType="inverted" className="button-container">Sign Up</Button>
+      </Form>  
+      </div>
+  );
+}
