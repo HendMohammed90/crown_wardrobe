e7d1b87c2a9e85ba289d46b4979f6ac92e4c6442
@hend.odesk.tasks <hend.odesk.tasks@gmail.com>
2024-12-03 10:44:28 +0200

    make use of add functionality into the cart

--- src/shared/Components/ProductCard/ProductCard.tsx
+++ src/shared/Components/ProductCard/ProductCard.tsx
@@ -1,12 +1,23 @@
 import { ShopProduct } from "@/shared/types/product"
 import Button from "../Button/Button"
 import "./product-card.styles.scss"
+import {CartContext} from "../../contexts/CartContext";
+import { useContext } from "react";
 
 type ProductCardProps = {
   product: ShopProduct
 }
 
 const ProductCard = ({ product }: ProductCardProps) => {
+
+  const { addToCart} = useContext(CartContext);
+
+  const handelAddToCart = ()=>{
+    console.log('Adding to the cart')
+    addToCart(product);
+  }
+
+
   return (
     <div className='product-card-container'>
       <img className="img" src={product.imageUrl} />
@@ -14,7 +25,7 @@ const ProductCard = ({ product }: ProductCardProps) => {
         <p className='name'>{product.name}</p>
         <span className='price'>{product.price}</span>
       </div>
-      <Button type="button" buttonType="inverted" className="button button-container">Add to card</Button>
+      <Button type="button" buttonType="inverted" className="button button-container" onClick={handelAddToCart}>Add to card</Button>
     </div>
   )
 }
