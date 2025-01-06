67495408abd8b743eaeafd6e8b3969a1e2c36983
@hend.odesk.tasks <hend.odesk.tasks@gmail.com>
2025-01-06 19:57:42 +0200

    add more descriptive types to retrived data

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,5 +1,19 @@
 export type category = {
-    id: number;
-    title: string;
-    imageUrl: string;
-  };
+  id: number;
+  title: string;
+  imageUrl: string;
+};
+
+
+export type Item = {
+  id: number;
+  name: string;
+  imageUrl: string;
+  price: number;
+}
+
+
+export type CategoryType = {
+  title: string;
+  items: Item[];
+}
